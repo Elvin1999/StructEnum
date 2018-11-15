999f0d46dbcd2041612652796ff7b4314beccff3
camalzade_elvin <camalzade_elvin@outlook.com>
2018-11-15 14:33:19 +0400

    Finished

--- StructEnum/Program.cs
+++ StructEnum/Program.cs
@@ -14,11 +14,25 @@ namespace StructEnum
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Manager = 160, Developer = 170, Intern = 80");
+            Console.WriteLine("Worker Position");
             string text = Console.ReadLine();
-
-
+            Console.WriteLine("Work hours per month");
+            int hours = Convert.ToInt32(Console.ReadLine());
             Accauntant accauntant = new Accauntant();
-            accauntant.AskForBonus(text, 150);
+            if (accauntant.AskForBonus(text, hours))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("You can get *Bonus*");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You can not get *Bonus*");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
 
         }
     }
