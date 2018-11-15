7a7bd149e5e2531fd062b473b447c4d018ab4964
camalzade_elvin <camalzade_elvin@outlook.com>
2018-11-15 14:24:54 +0400

    Add project files.

---
+++ StructEnum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructEnum
+{
+    enum WorkerPosition
+    {
+        Manager = 160, Developer = 170, Intern = 80
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+
+            Accauntant accauntant = new Accauntant();
+            accauntant.AskForBonus(text, 150);
+
+        }
+    }
+    struct Accauntant
+    {
+
+        public bool AskForBonus(string worker, int hours)
+        {
+
+            var a = worker.Trim();
+            var b = a.ToLower();
+            if (b == "developer" && hours >= (int)WorkerPosition.Developer)
+            {
+                return true;
+            }
+            else if (b == "manager" && hours >= (int)WorkerPosition.Manager)
+            {
+                return true;
+            }
+            else if (b == "intern" && hours >= (int)WorkerPosition.Intern)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
